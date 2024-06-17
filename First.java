749f54a1cdb5898d366bbd64e7b4cb7e5873536f
prabir7 <103267402+prabir7@users.noreply.github.com>
2024-06-17 22:21:30 +0530

    Create First.java
    First Java test file.

---
+++ First.java
@@ -0,0 +1,6 @@
+public class First{
+  public static void main(String args[]){
+    System.out.println("This code is running from Prabir GitHub Repo");
+    System.out.println("This is now end of scirpt. Thaks from GitHub");
+  }
+}
